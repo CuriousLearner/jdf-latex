4e458106c8cb582ee5494bdc8bdee13661887f3d
GeekyShacklebolt <shivasaxena911@gmail.com>
2019-05-27 21:00:22 +0530

    fix(enumerate-refs): Fix reference enumeration for both author & non-author bibliography

--- {{cookiecutter.project_name}}/enumerate-refs.py
+++ {{cookiecutter.project_name}}/enumerate-refs.py
@@ -4,17 +4,30 @@
 
 
 def apply_numbers_to_ref_list():
-    count = 1
+    count = 0
     write_lines = []
+
     with open('./main.bbl', 'r+') as file:
         read_lines = file.readlines()
         write_lines = read_lines[:]
+
+        enumerated = None
+
         for i in range(len(read_lines) - 1):
-            if "\APACrefbtitle" in read_lines[i]:
+            if "bibitem" in read_lines[i]:
+                # Mark the beginning of a new bibliography item
+                count = count + 1
+                enumerated = False
+            elif not enumerated and "begin{APACrefauthors}" in read_lines[i]:
+                write_lines[i + 1] = (
+                    '\hspace*{7.3mm}' + str(count) + '.\hspace*{2.4mm}' + write_lines[i + 1]
+                )
+                enumerated = True
+            elif not enumerated and "\APACrefbtitle" in read_lines[i]:
                 write_lines[i] = (
                     '\hspace*{7.3mm}' + str(count) + '.\hspace*{2.4mm}' + write_lines[i]
                 )
-                count = count + 1
+
         file.seek(0)
         file.writelines(write_lines)
 
